03e7cdfb038e022ab8b53844a30e96ec665fdbc7
navdeep dhakar <navdepdhakar1@gmail.com>
2023-07-12 10:08:57 +0530

    added gunicorn for better prod deployment

--- streamai/app.py
+++ streamai/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import uvicorn
+import subprocess
 import random, string
 from streamai.llms import Autoalpacalora
 app = FastAPI()
@@ -21,12 +22,22 @@ class endpointIO:
     def __init__(self, iofunc):
         self.iopipe = iofunc 
 
-    def run(self):
+    def run(self, port=8080):
         print("starting AI services")
         print(f"inference endpoint -> http://0.0.0.0:8000/{endpointsecret}/inference")
         @app.post(f"/xFlwFmy8qUlNUMibglik/inference")
         async def root(data:Data):
             output = self.iopipe(data.input)
             return {"return": f"{output}"}
-        uvicorn.run(app, host="0.0.0.0", port=8000)  # Run FastAPI server
+        gunicorn_command = [
+            "gunicorn",
+            "main:app",
+            "--workers",
+            "4",
+            "--worker-class",
+            "uvicorn.workers.UvicornWorker",
+            "--bind",
+            f"0.0.0.0:{port}"
+        ]
+        subprocess.run(gunicorn_command)
 
--- test.py
+++ test.py
@@ -4,11 +4,10 @@ from streamai.llms import Autoalpacalora
 def testiofunc(inpt:str):
     return f"this is output of {inpt}"
     
-modelinstance = Autoalpacalora("decapoda-research/llama-7b-hf")
-modelinstance.loadmodel() #required for deployment of model as api, not required during finetuning.
+#modelinstance = Autoalpacalora("decapoda-research/llama-7b-hf")
+#modelinstance.loadmodel() #required for deployment of model as api, not required during finetuning.
 #modelinstance.setparameters(input="use this as context", max_tokens=128, top_p=12, top_k=40) #optional, look into .info['available_methods']['setparameters'] for more details.
 #print(modelinstance.info['available_methods'])
-output = modelinstance.inferenceIO(prompt="what is llama?")
-print(output)
-#model1 = endpointIO(modelinstance.testinferenceIO)
-#model1.run()
+#output = modelinstance.inferenceIO(prompt="what is llama?"):
+model1 = endpointIO(testinferenceIO)
+model1.run()
