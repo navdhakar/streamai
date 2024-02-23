4fedd404179362a784ee230dcc1fd08ce8fcb37b
corporaai <corporaapp@gmail.com>
2024-02-23 17:41:14 +0530

    added local dataset file support

--- streamai/utils/download.py
+++ streamai/utils/download.py
@@ -5,15 +5,21 @@ import json
 from tqdm import tqdm
 import os
 def download_file(url, local_filename):
-    # NOTE the stream=True parameter below
-    print("downloading train dataset...")
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192): 
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                #if chunk: 
-                f.write(chunk)
-    print("download finished")
+    try:
+        # Check if the URL is a local path or filename
+        if os.path.exists(url):  # If it's a local path or filename
+            local_filename = url
+            print(f"Using local file: {local_filename}")
+        else:  # If it's a URL
+            print("Downloading file from URL...")
+            with requests.get(url, stream=True) as r:
+                r.raise_for_status()
+                with open(local_filename, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=8192):
+                        f.write(chunk)
+            print("Download finished")
+    except Exception as e:
+        print("Error:", e)
+        print("URL is not correct. If it is a local file, please check the path provided.")
+        return None
     return local_filename
--- test/serveagent.py
+++ test/serveagent.py
@@ -4,5 +4,7 @@ def testiofunc(inpt:str):
     output = modelinstance.inferenceIO(prompt=input)
 modelinstance = Autoalpacalora("decapoda-research/llama-7b-hf")
 modelinstance.loadmodel() #required for deployment of model as api, not required during finetuning.
+#if api endpoint is not accepting request, please check your server or vpc firewall.
+#it seems to work with anychange in some clusters but need ufw firewall modification in some.(last tested on lamda lab rtx 6000)
 model1 = endpointIO(modelinstance.inferenceIO)
 model1.run()
