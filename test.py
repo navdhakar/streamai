de189791b4a9ce7060282429d83535d00f4cd972
Navdeep Dhakar <navdepdhakar1@gmail.com>
2024-03-06 09:20:23 +0530

    bugs

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ modelinstance = Autoalpacalora(base_model="decapoda-research/llama-7b-hf")
 print(json.dumps(modelinstance.info['dataset'], indent = 1))
 
 #let's provide the dataset url
-modelinstance.train(dataset_url="https://firebasestorage.googleapis.com/v0/b/pdf-analysis-saas.appspot.com/o/Other%2Fdataset.json?alt=media&token=28abd658-a308-4050-b631-54bab9b63a6b"m num_train_epochs=5, max_length=512) # here it is using dummy dataset available on this link.
+modelinstance.train(dataset_url="https://firebasestorage.googleapis.com/v0/b/pdf-analysis-saas.appspot.com/o/Other%2Fdataset.json?alt=media&token=28abd658-a308-4050-b631-54bab9b63a6b" num_train_epochs=5, max_length=512) # here it is using dummy dataset available on this link.
 
 #then load fine tuned model using lora weights
 modelinstance.loadmodel(lora_weights="alpacafinetuned")
